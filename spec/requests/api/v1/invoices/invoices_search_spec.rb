8d1493d43266c8e9895b5ee9c7302d5d989ecc84
BLaurenB <blbillington1@gmail.com>
2017-11-28 15:49:31 -0700

    find working

--- spec/requests/api/v1/invoices/invoices_search_spec.rb
+++ spec/requests/api/v1/invoices/invoices_search_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Search Invoices using find and parameters" do
-  it "returns a list of invoices" do
+  it "returns an invoice based on the id" do
     create_list(:invoice, 3)
     invoice = Invoice.last
 
@@ -12,9 +12,50 @@ describe "Search Invoices using find and parameters" do
 
     expect(invoice).to have_key(:id)
     expect(invoice).to have_key(:status)
-    expect(invoice).to have_key(:created_at)
 
   end
 
+  it "returns an invoice based on the status" do
+    create_list(:invoice, 3)
+    invoice = Invoice.last
+
+    get "/api/v1/invoices/find?status=#{invoice.status}"
+    expect(response).to be_success
+
+    invoice = JSON.parse(response.body, symbolize_names: true)
+
+    expect(invoice).to have_key(:id)
+    expect(invoice).to have_key(:status)
+
+  end
+
+  xit "returns an invoice based on the created_at date time" do
+    create_list(:invoice, 3)
+    invoice = Invoice.last
+
+    byebug
+
+    get "/api/v1/invoices/find?created_at=#{invoice.created_at}"
+    expect(response).to be_success
+
+    invoice = JSON.parse(response.body, symbolize_names: true)
+
+    expect(invoice).to have_key(:id)
+    expect(invoice).to have_key(:status)
+
+  end
+
+  it "returns an invoice based on a random selection" do
+    create_list(:invoice, 3)
+    invoice = Invoice.last
+
+    get "/api/v1/invoices/random?"
+    expect(response).to be_success
+
+    invoice = JSON.parse(response.body, symbolize_names: true)
+
+    expect(invoice).to have_key(:id)
+    expect(invoice).to have_key(:status)
+  end
 
 end
