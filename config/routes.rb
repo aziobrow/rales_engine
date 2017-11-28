a67417050f18e29ba206195736bb7ed15d34c97a
BLaurenB <blbillington1@gmail.com>
2017-11-28 08:52:48 -0700

    worked on custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :invoices, only: [:index]
+      resources :invoices, only: [:index, :show]
+      namespace :invoices do
+        controller :search do
+          get "/find" => :show
+          get "/find_all" => :index
+        end
+      end
     end
   end
+
+
+
 end
---
+++ spec/requests/api/v1/invoices/invoices_search_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Search Invoices using find and parameters" do
+  it "returns a list of invoices" do
+    create_list(:invoice, 3)
+    invoice = Invoice.last
+
+    get "/api/v1/invoices/find?id=#{invoice.id}"
+    expect(response).to be_success
+
+    # invoice = JSON.parse(response.body, symbolize_names: true)
+    #
+    # expect(invoice).to have_key(:id)
+    # # byebug
+    # expect(invoice).to have_key(:status)
+    # expect(invoice).to have_key(:created_at)
+
+  end
+
+
+end
