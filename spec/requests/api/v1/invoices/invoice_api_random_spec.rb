3d9ab6bd266b46306c157103418ebfe5df9b0ec9
BLaurenB <blbillington1@gmail.com>
2017-11-28 22:08:23 -0700

    refactoring random and tests

---
+++ spec/requests/api/v1/invoices/invoice_api_random_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Search Invoices using find and parameters" do
+
+  it "returns an invoice based on a random selection" do
+    create_list(:invoice, 3)
+    @invoice = Invoice.last
+
+    get "/api/v1/invoices/random"
+    expect(response).to be_success
+
+    invoice = JSON.parse(response.body, symbolize_names: true)
+
+    expect(invoice).to have_key(:id)
+    expect(invoice).to have_key(:status)
+    expect(invoice).to have_key(:merchant_id)
+    expect(invoice).to have_key(:customer_id)
+  end
+
+end
