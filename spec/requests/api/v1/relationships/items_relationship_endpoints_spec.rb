2a29fe63df558f6134f7706bfa468dcd4376892f
Aurora Ziobrowski <aziobrow@gmail.com>
2017-11-29 18:16:19 -0700

    fixed another merge

--- spec/requests/api/v1/relationships/items_relationship_endpoints_spec.rb
+++ spec/requests/api/v1/relationships/items_relationship_endpoints_spec.rb
@@ -9,33 +9,21 @@ describe "Items Relationship Endpoints API" do
     @invoice_item2 = create(:invoice_item, item_id: @item.id, invoice_id: invoice.id)
   end
 
-<<<<<<< HEAD
     it "finds associated invoice items for one item" do
       get "/api/v1/items/#{@item.id}/invoice_items"
       invoice_items = JSON.parse(response.body, symbolize_names: true)
-=======
-  it "finds associated invoice items for one item" do
-    get "/api/v1/items/:id/invoice_items"
-    invoice_items = JSON.parse(response.body, symbolize_names: true)
->>>>>>> 075344c79d5b305dd7ec5363d0d40f09b675fc0c
 
-    expect(response).to be_success
-    expect(invoice_items.count).to eq(2)
-    expect(invoice_items.first).to have_value(@invoice_item1.id)
-    expect(invoice_items.last).to have_value(@invoice_item2.id)
+      expect(response).to be_success
+      expect(invoice_items.count).to eq(2)
+      expect(invoice_items.first).to have_value(@invoice_item1.id)
+      expect(invoice_items.last).to have_value(@invoice_item2.id)
   end
 
-<<<<<<< HEAD
     it "finds associated merchant for one item" do
       get "/api/v1/items/#{@item.id}/merchant"
       merchant = JSON.parse(response.body, symbolize_names: true)
-=======
-  it "finds associated merchant for one item" do
-    get "/api/v1/items/:id/merchant"
-    merchant = JSON.parse(response.body, symbolize_names: true)
->>>>>>> 075344c79d5b305dd7ec5363d0d40f09b675fc0c
 
-    expect(response).to be_success
-    expect(merchant).to have_value(@merchant.id)
+      expect(response).to be_success
+      expect(merchant).to have_value(@merchant.id)
   end
 end
