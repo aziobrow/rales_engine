7c6c6005348c866f4b75bc1b60e114ededeab5e5
BLaurenB <blbillington1@gmail.com>
2017-11-30 17:37:39 -0700

    created method to fix unit_price search params

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -5,12 +5,15 @@ class Api::V1::InvoiceItems::SearchController < ApplicationController
   end
 
   def show
+    # byebug
     render json: InvoiceItem.find_by(search_params)
   end
 
 private
 
   def search_params
+
+    params[:unit_price] = (params[:unit_price].to_f * 100).to_i if params[:unit_price]
     params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at)
   end
 
